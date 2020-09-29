a493727007d0f8e5e24fe795bb83e589e8733d7f
HiDoYa <hiroyagojo@gmail.com>
2020-09-28 21:33:11 -0700

    Tweet path resolved

--- tweet.py
+++ tweet.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+import pathlib
 import sys
 import os
 import random
 import tweepy
-
-if len(sys.argv) != 2:
-    print("Usage: ./tweet.py /path/to/dir")
-    exit()
 
 access_token = os.environ['TW_ACCESS_TOKEN']
 access_token_secret = os.environ['TW_ACCESS_TOKEN_SECRET']
@@ -17,9 +14,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 tw = tweepy.API(auth)
 
-ls = os.listdir(os.path.join(sys.argv[1], "tama_images"))
+parent_path = str(pathlib.Path(__file__).parent.resolve())
+
+ls = os.listdir(os.path.join(parent_path, "tama_images"))
 selected_im = random.choice(ls)
 
-im_handler = tw.media_upload(os.path.join(sys.argv[1],"tama_images/", selected_im))
+im_handler = tw.media_upload(os.path.join(parent_path, "tama_images/", selected_im))
 media_ids = [im_handler.media_id_string]
 status = tw.update_status("Today's tamagotchi!", media_ids=media_ids, source="random user")
