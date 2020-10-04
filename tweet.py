1a4b01be1b983a16f665e6990ff5f1389970dd29
HiDoYa <hiroyagojo@gmail.com>
2020-10-03 21:06:06 -0700

    Added support for jp and en name

--- tweet.py
+++ tweet.py
@@ -4,6 +4,21 @@ import os
 import random
 import tweepy
 
+def get_name(im):
+    jp_name = ""
+    en_name = ""
+    im = im.replace("jpg", "txt")
+    parent_path = str(pathlib.Path(__file__).parent.resolve())
+    jp_dir = os.path.join(parent_path, "name_tama_images_meta_jp")
+    en_dir = os.path.join(parent_path, "name_tama_images_meta_en")
+    with open(os.path.join(jp_dir, im), "r") as f:
+        jp_name = f.read()
+
+    with open(os.path.join(en_dir, im), "r") as f:
+        en_name = f.read()
+
+    return jp_name, en_name
+
 access_token = os.environ['TW_ACCESS_TOKEN']
 access_token_secret = os.environ['TW_ACCESS_TOKEN_SECRET']
 consumer_key = os.environ['TW_API_KEY']
@@ -15,9 +30,11 @@ tw = tweepy.API(auth)
 
 parent_path = str(pathlib.Path(__file__).parent.resolve())
 
-ls = os.listdir(os.path.join(parent_path, "tama_images"))
+ls = os.listdir(os.path.join(parent_path, "imag_tama_images"))
 selected_im = random.choice(ls)
 
-im_handler = tw.media_upload(os.path.join(parent_path, "tama_images/", selected_im))
-media_ids = [im_handler.media_id_string]
-status = tw.update_status("Today's tamagotchi!", media_ids=media_ids, source="random user")
+jp_name, en_name = get_name(selected_im)
+im_handler1 = tw.media_upload(os.path.join(parent_path, "imag_tama_images/", selected_im))
+im_handler2 = tw.media_upload(os.path.join(parent_path, "tama_images/", selected_im))
+media_ids = [im_handler1.media_id_string, im_handler2.media_id_string]
+status = tw.update_status("Today's tamagotchi!: {} / {}".format(jp_name, en_name), media_ids=media_ids, source="random user")
--- utility/run.sh
+++ utility/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-python3 links.py
-python3 cut.py
-python3 label.py
-python3 correction.py create
+
+if [ $1 = "create" ]; then
+	python3 links.py
+	python3 cut.py
+	python3 label.py
+	python3 correction.py create
+fi
+
+echo $1
+if [ $1 = "load" ]; then
+	python3 correction.py load
+	cp -r name_tama_images_meta_en ../
+	cp -r name_tama_images_meta_jp ../
+	cp -r imag_tama_images ../
+	cp -r tama_images ../
+fi
